8730a887cf18a7af92bb125d81c9a9836a2f8d2a
Terry <terry_turpening@hotmail.com>
2016-02-28 19:47:03 -0800

    Initial push

---
+++ FreezerRackBox/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lat = new List<string> { "A", "B" };
+            List<string> lng = new List<string> { "1", "2" };
+
+            List<Freezer> freezer = new List<Freezer>();
+        }
+    }
+    class Freezer
+    {
+        public string Rack { get; set; }
+        public string Box { get; set; }
+        public string Location { get; set; }
+    }
+}
+
