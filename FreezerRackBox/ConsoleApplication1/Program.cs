3cbb914223c7f58348349ba2508040d1dc9e328a
Terry <terry_turpening@hotmail.com>
2016-02-28 20:09:03 -0800

    Minor update simple list work

--- FreezerRackBox/ConsoleApplication1/Program.cs
+++ FreezerRackBox/ConsoleApplication1/Program.cs
@@ -10,10 +10,20 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            List<string> rack = new List<string> { "LN3", "LN3" };
+            List<string> box = new List<string> { "1", "2", "3" };
             List<string> lat = new List<string> { "A", "B" };
             List<string> lng = new List<string> { "1", "2" };
 
             List<Freezer> freezer = new List<Freezer>();
+
+            foreach (var _box in box.Select((value, index) => new { value, index }))
+            {
+                Console.WriteLine(_box.value);
+                Console.WriteLine("count value: " +_box.index.ToString());               
+            }
+            Console.WriteLine("Press any key to continue . . .");
+            Console.ReadKey();
         }
     }
     class Freezer
